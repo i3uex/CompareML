5b88ede5c4db866701bf6682d40064ca429b1736
i3uex <javiersierrablazquez@gmail.com>
2019-11-02 13:03:19 +0100

    feat: (front-end) Implement dynamic target feature selection
    Features have been taken from first line of csv.

--- public/client.js
+++ public/client.js
@@ -1,25 +1,36 @@
-$(document).ready(function () {
-    $("#upload_form").submit(function (e) {
-        e.preventDefault();
+function submitFile(e) {
+    e.preventDefault();
+    var reader = new FileReader();
+    reader.onload = function () {
+        console.log(reader.result);
+        populateTargetSelect(reader.result.split('\n')[0]);
 
-        var reader = new FileReader();
-        reader.onload = function () {
-            console.log(reader.result)
-            $.ajax({
-                type: "POST",
-                url: "/load_csv",
-                data: {
-                    csv: 'test'
-                },
-                success: function (result) {
-                    alert('done' + result);
-                },
-                error: function (result) {
-                    alert('fail');
-                }
-            });
+        // Upload file
+        $.ajax({
+            type: "POST",
+            url: "/load_csv",
+            data: {
+                csv: reader.result
+            },
+            success: function (result) {
+                console.log('done');
+            },
+            error: function (result) {
+                alert('fail');
+            }
+        });
+    };
+    reader.readAsText($("#file")[0].files[0]);
+    
+    return false;
+};
 
-        };
-        reader.readAsText($("#file")[0].files[0]);
+function populateTargetSelect(features) {
+    var select = $("#target_select");
+    features.split(',').forEach(feature => {
+        var option = document.createElement("option");
+        option.text = feature;
+        option.id = feature;
+        select.append(option);
     });
-});
+}
