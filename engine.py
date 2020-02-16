f9c7c8c86e0bf6d9c3a45314308f20c7c1302180
i3uex <javiersierrablazquez@gmail.com>
2020-02-16 12:04:11 +0100

    feat: (back-end) Implement OneHotEncoding for categorical features
    It solves sklearn error on categorical features

--- engine.py
+++ engine.py
@@ -1,7 +1,9 @@
 from io import StringIO
 
 import pandas
+from pandas.api.types import is_numeric_dtype
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 
 import constants as c
 import providers.r as r
@@ -39,9 +41,14 @@ def getAlgorithms():
     return ALGORITHMS
 
 
-def split_dataset(dataset: str, target: str):
+def _split_dataset(dataset: str, target: str):
     features_values = pandas.read_csv(StringIO(dataset))
     target_values = features_values.pop(target)
+
+    # Do OneHotEncoding for categorical features:
+    features_values = _doOneHotEncoding(features_values)
+
+    # Split
     features_train, features_test, labels_train, labels_test = train_test_split(features_values, target_values,
                                                                                 test_size=0.2)
     # Add target label
@@ -57,8 +64,24 @@ def split_dataset(dataset: str, target: str):
     return features_train, features_test, labels_train, labels_test
 
 
+def _doOneHotEncoding(features_values):
+    for col in features_values:
+        if not is_numeric_dtype(features_values[col]):
+            # One Hot Encoding
+            encoder = OneHotEncoder(handle_unknown='ignore')
+            encoded_df = pandas.DataFrame(encoder.fit_transform(features_values[[col]]).toarray())
+            encoded_df = encoded_df.add_prefix(col + '_')
+
+            features_values = features_values.join(encoded_df)
+
+            # Drop original column
+            features_values = features_values.drop(col, axis=1)
+
+    return features_values
+
+
 def execute(dataset: str, providers: [], algorithms: [], target: str):
-    features_train, features_test, labels_train, labels_test = split_dataset(dataset, target)
+    features_train, features_test, labels_train, labels_test = _split_dataset(dataset, target)
 
     results = {}
     for algorithm in algorithms:
