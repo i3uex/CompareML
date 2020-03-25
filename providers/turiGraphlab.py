5ee701b7a17889455009d688fc7dc423c75d40a8
Juan Diego Gutiérrez Gallardo <andy@unex.es>
2020-03-25 13:31:54 +0100

    Create containers for new algorithms

--- providers/turiGraphlab.py
+++ providers/turiGraphlab.py
@@ -15,6 +15,10 @@ def execute(
 ):
     if algorithm == c.RANDOM_FOREST:
         return _random_forest(features_train, features_test, labels_train, labels_test, target)
+    elif algorithm == c.LOGISTIC_REGRESSION:
+        return _logistic_regression(features_train, features_test, labels_train, labels_test, target)
+    elif algorithm == c.NEURAL_NETWORK_MP:
+        return _neural_network_mp(features_train, features_test, labels_train, labels_test, target)
     else:
         # TODO: raise error
         pass
@@ -44,6 +48,31 @@ def _random_forest(
         results['confusion_matrix'] = '"\n' + str(results['confusion_matrix']) + '"'
 
     return results
+
+
+def _logistic_regression(
+    features_train: pandas.DataFrame,
+    features_test: pandas.DataFrame,
+    labels_train: pandas.DataFrame,
+    labels_test: pandas.DataFrame,
+    target: str
+):
+    train_data_sf, test_data_sf = _get_sframes(features_train, features_test, labels_train, labels_test)
+
+    return ""
+
+
+def _neural_network_mp(
+    features_train: pandas.DataFrame,
+    features_test: pandas.DataFrame,
+    labels_train: pandas.DataFrame,
+    labels_test: pandas.DataFrame,
+    target: str
+):
+    train_data_sf, test_data_sf = _get_sframes(features_train, features_test, labels_train, labels_test)
+
+    return ""
+
 
 def _get_sframes(features_train, features_test, labels_train, labels_test):
     train_data: pandas.DataFrame = features_train.join(labels_train)
