8da36359a51879bbafac6b25fd798c60d53a6a1e
Juan Diego Gutiérrez Gallardo <andy@unex.es>
2020-03-26 07:12:33 +0100

    Use hyperparameter value from constants

--- providers/scikitLearn.py
+++ providers/scikitLearn.py
@@ -15,6 +15,10 @@ def execute(
 ):
     if algorithm == c.RANDOM_FOREST:
         return _random_forest(features_train, features_test, labels_train, labels_test)
+    elif algorithm == c.LOGISTIC_REGRESSION:
+        return _logistic_regression(features_train, features_test, labels_train, labels_test)
+    elif algorithm == c.NEURAL_NETWORK_MP:
+        return _neural_network_mp(features_train, features_test, labels_train, labels_test)
     else:
         # TODO: raise error
         pass
@@ -26,8 +30,28 @@ def _random_forest(
         labels_train: pandas.DataFrame,
         labels_test: pandas.DataFrame,
 ):
-    rfc = RandomForestClassifier()
+    rfc = RandomForestClassifier(
+        max_depth=c.RF_MAX_DEPTH
+    )
     rfc.fit(features_train, labels_train)
     rfc_predictions = rfc.predict(features_test)
 
     return classification_report(labels_test, rfc_predictions, output_dict=True)
+
+
+def _logistic_regression(
+        features_train: pandas.DataFrame,
+        features_test: pandas.DataFrame,
+        labels_train: pandas.DataFrame,
+        labels_test: pandas.DataFrame
+):
+    return ""
+
+
+def _neural_network_mp(
+        features_train: pandas.DataFrame,
+        features_test: pandas.DataFrame,
+        labels_train: pandas.DataFrame,
+        labels_test: pandas.DataFrame
+):
+    return ""
