858a10f5a4ea5d4aca984117953f47740c1eda42
Juan Diego Gutiérrez Gallardo <andy@unex.es>
2020-04-21 20:28:18 +0200

    Move configuration to INI files

--- main.py
+++ main.py
@@ -21,42 +21,10 @@ if __name__ == '__main__':
     if not os.path.exists('log'):
         os.makedirs('log')
 
-    server_conf = {
-        'server.socket_host': '0.0.0.0',
-        'server.socket_port': 8080,
-        'log.access_file': 'log/access.log',
-        'log.error_file': 'log/error.log',
-        'environment': 'production',
-    }
-
-    app_conf = {
-        '/': {
-            'tools.staticdir.root': os.path.abspath(os.getcwd()),
-        },
-        '/get_options': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        },
-        '/set_options': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        },
-        '/get_default_dataset_headers': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'application/json')],
-        },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': 'public'
-        }
-    }
-    cherrypy.config.update(server_conf)
+    cherrypy.config.update("global.ini")
 
     webapp = WebServer()
     webapp.get_options = GetOptionsService()
     webapp.set_options = SetOptionsService()
     webapp.get_default_dataset_headers = GetDefaultDatasetHeadersService()
-    cherrypy.quickstart(webapp, '/', app_conf)
+    cherrypy.quickstart(webapp, '/', "app.ini")
