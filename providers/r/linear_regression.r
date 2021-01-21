8981f95c23f818315f3a1abf4609b38cf1fd0250
ajfernandez84 <ajfernandez@ual.es>
2021-01-21 22:45:54 +0100

    Update linear_regression.r

--- providers/r/linear_regression.r
+++ providers/r/linear_regression.r
@@ -65,6 +65,6 @@ predictionsLR <- predict(modelLR, newdata = test)
 test$lr = predictionsLR
 rmseLR = sqrt(mean((as.numeric(test$lr)-as.numeric(test[[target]]))^2))
 maxerrorLR = max(as.numeric(test$lr)-as.numeric(test[[target]]))
-result <- paste("rmse:", rmseLR, ":max_error:", maxerrorLR, ":predicciones:", predictionsLR[1:5], sep = "")
+result <- paste("rmse:", rmseLR, ":max_error:", maxerrorLR, ":predicciones:", test[[target]][1:5], sep = "")
 #result <- paste("a:", test[[target]][1], ":b:", test$lr[1], sep = "")
 cat(result)
