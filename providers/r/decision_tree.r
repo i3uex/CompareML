fd3abee784889162d828ae7efa59a6a8d1d758b2
ajfernandez84 <ajfernandez@ual.es>
2021-01-21 13:38:42 +0100

    Update decision_tree.r

--- providers/r/decision_tree.r
+++ providers/r/decision_tree.r
@@ -11,7 +11,7 @@ max_reg <- function(model_obj, testing = NULL, target = NULL) {
     # target  - target variable (length 1 character vector)
     yhat <- predict(model_obj, newdata = testing)
     actual <- testing[[target]]
-    max(yhat-actual)
+    max(as.numeric(as.character(yhat))-as.numeric(as.character(actual)))
 }
 
 rmse_reg <- function(model_obj, testing = NULL, target = NULL) {
@@ -21,7 +21,7 @@ rmse_reg <- function(model_obj, testing = NULL, target = NULL) {
     # target  - target variable (length 1 character vector)
     yhat <- predict(model_obj, newdata = testing)
     actual <- testing[[target]]
-    sqrt(mean((yhat-actual)^2))
+    sqrt(mean((as.numeric(as.character(yhat))-as.numeric(as.character(actual)))^2))
 }
 
 
