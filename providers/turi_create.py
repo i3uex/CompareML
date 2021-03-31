95414dbaeb600e58e84eacafeaf2b0bc84f86b99
Juan Diego Gutiérrez Gallardo <andy@unex.es>
2021-03-31 20:34:41 +0200

    Show r2_score in Turi Create

--- providers/turi_create.py
+++ providers/turi_create.py
@@ -18,7 +18,8 @@ def execute(
 ):
     logging.debug(f"turi.execute()")
     try:
-        train_data_sf, test_data_sf = _get_sframes(features_train, features_test, labels_train, labels_test)
+        train_data_sf, test_data_sf = _get_sframes(
+            features_train, features_test, labels_train, labels_test)
         if algorithm == c.LINEAR_REGRESSION:
             return _linear_regression(train_data_sf, test_data_sf, target)
         elif algorithm == c.BOOSTED_DECISION_TREES:
@@ -51,10 +52,12 @@ def _linear_regression(
         target=target,
         verbose=False
     )
+    lr_predictions = model.predict(test_data_sf)
 
     # Evaluate the model and save the results into a dictionary
     results = model.evaluate(test_data_sf)
-    results = _process_results(results)
+    results = _process_results(
+        results, test_data_sf, lr_predictions, c.LINEAR_REGRESSION)
 
     return results
 
@@ -73,10 +76,12 @@ def _boosted_decision_trees(
         verbose=False,
         max_iterations=c.BDT_MAX_ITERATIONS
     )
+    btr_predictions = model.predict(test_data_sf)
 
     # Evaluate the model and save the results into a dictionary
     results = model.evaluate(test_data_sf)
-    results = _process_results(results)
+    results = _process_results(
+        results, test_data_sf, btr_predictions, c.BOOSTED_DECISION_TREES)
 
     return results
 
@@ -95,10 +100,12 @@ def _decision_tree(
         max_depth=c.DT_MAX_DEPTH,
         verbose=False
     )
+    dtr_predictions = model.predict(test_data_sf)
 
     # Evaluate the model and save the results into a dictionary
     results = model.evaluate(test_data_sf)
-    results = _process_results(results)
+    results = _process_results(
+        results, test_data_sf, dtr_predictions, c.DECISION_TREE)
 
     return results
 
@@ -181,11 +188,41 @@ def _get_sframes(features_train, features_test, labels_train, labels_test):
     return train_data_sf, test_data_sf
 
 
-def _process_results(results):
+def _process_results(
+        results,
+        test_data_sf=None,
+        predictions=None,
+        algorithm=None):
     logging.debug(f"turi._process_results()")
 
     if 'roc_curve' in results:
         del results['roc_curve']
     if 'confusion_matrix' in results:
         results['confusion_matrix'] = '"\n' + str(results['confusion_matrix']) + '"'
+
+    if algorithm == c.LINEAR_REGRESSION or algorithm == c.BOOSTED_DECISION_TREES or algorithm == c.DECISION_TREE:
+        r2_score = _get_r2_score(test_data_sf, predictions)
+        results['r2_score'] = r2_score
+
     return results
+
+
+def _get_r2_score(test_data_sf, y_pred):
+    logging.debug(f"turi._get_r2_score()")
+
+    column_names = test_data_sf.column_names()
+    last_column_name = column_names[-1]
+    y_true = test_data_sf.select_column(last_column_name)
+
+    rss = ((y_pred - y_true) ** 2).sum()
+    tss = ((y_true - y_true.mean()) ** 2).sum()
+    rsq = 1 - rss / tss
+
+    return rsq
+
+    # preds < - c(1, 2, 3)
+    # actual < - c(2, 2, 4)
+    # rss < - sum((preds - actual) ^ 2)
+    # tss < - sum((actual - mean(actual)) ^ 2)
+    # rsq < - 1 - rss / tss
+
